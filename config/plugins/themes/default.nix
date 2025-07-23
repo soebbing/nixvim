823c253b6b616aec998bfcd89e8a9e969443063b
Hendrik Söbbing <hendrik@soebbing.de>
2025-07-23 20:30:31 +0200

    Fix indent

--- config/plugins/themes/default.nix
+++ config/plugins/themes/default.nix
@@ -3,41 +3,42 @@
     enable = true;
 
     settings = {
-    colors = {
-      palette = {
-        fujiWhite = "#FFFFFF";
-        sumiInk0 = "#000000";
-      };
-      theme = {
-        all = {
-          ui = {
-            bg_gutter = "none";
-          };
+      colors = {
+        palette = {
+          fujiWhite = "#FFFFFF";
+          sumiInk0 = "#000000";
         };
-        dragon = {
-          syn = {
-            parameter = "yellow";
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none";
+            };
           };
-        };
-        wave = {
-          ui = {
-            float = {
-              bg = "none";
+          dragon = {
+            syn = {
+              parameter = "yellow";
+            };
+          };
+          wave = {
+            ui = {
+              float = {
+                bg = "none";
+              };
             };
           };
         };
       };
+      commentStyle = {
+        italic = true;
+      };
+      compile = false;
+      dimInactive = false;
+      functionStyle = { };
+      overrides = "function(colors) return {} end";
+      terminalColors = true;
+      theme = "wave";
+      transparent = false;
+      undercurl = true;
     };
-    commentStyle = {
-      italic = true;
-    };
-    compile = false;
-    dimInactive = false;
-    functionStyle = { };
-    overrides = "function(colors) return {} end";
-    terminalColors = true;
-    theme = "wave";
-    transparent = false;
-    undercurl = true;
   };
 }
