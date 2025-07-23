cf9f5174b51fd4d19a5cd61a65387e18701e3a48
Hendrik Söbbing <hendrik@soebbing.de>
2025-07-23 18:41:08 +0200

    Switch to Osaka

--- config/plugins/themes/default.nix
+++ config/plugins/themes/default.nix
@@ -1,9 +1,29 @@
 {
-  colorschemes.solarized = {
+  colorschemes.solarized-osaka = {
     enable = true;
 
     settings = {
-      background = "light";
+      style = "light";
+      transparent = false;
+
+      integrations = {
+        treesitter = true;
+        telescope = true;
+        gitsigns = true;
+        cmp = true;
+        which_key = true;
+
+        native_lsp = {
+          enabled = true;
+          virtual_text = {
+            errors = [ "italic" ];
+            hints = [ "italic" ];
+          };
+          underlines = {
+            errors = [ "underline" ];
+          };
+        };
+      };
     };
   };
 }
