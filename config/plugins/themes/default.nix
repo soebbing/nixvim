cb4b79415ff4f27d9cfcf7adf62766b0d73af48a
Hendrik Söbbing <hendrik@soebbing.de>
2025-07-23 20:40:09 +0200

    Gruvbox

--- config/plugins/themes/default.nix
+++ config/plugins/themes/default.nix
@@ -1,44 +1,10 @@
 {
-  colorschemes.kanagawa = {
+  colorschemes.gruvbox = {
     enable = true;
 
     settings = {
-      colors = {
-        palette = {
-          fujiWhite = "#FFFFFF";
-          sumiInk0 = "#000000";
-        };
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none";
-            };
-          };
-          dragon = {
-            syn = {
-              parameter = "yellow";
-            };
-          };
-          wave = {
-            ui = {
-              float = {
-                bg = "none";
-              };
-            };
-          };
-        };
-      };
-      commentStyle = {
-        italic = true;
-      };
-      compile = false;
-      dimInactive = false;
-      functionStyle = { };
-      overrides = "function(colors) return {} end";
-      terminalColors = true;
-      theme = "wave";
-      transparent = false;
-      undercurl = true;
+      background = "light";
+      terminal_colors = true;
     };
   };
 }
